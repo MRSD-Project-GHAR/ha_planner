e32135918fbbcaf6eb51452e9f1d6c969b81250f
Kshitij Kabeer <kshitijkabeer@gmail.com>
2023-11-06 18:05:08 -0500

    Get start and goal pose via clicked point

--- mbf_rrts_planner/src/rrts_mb_action_server.cpp
+++ mbf_rrts_planner/src/rrts_mb_action_server.cpp
@@ -8,6 +8,7 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <std_srvs/Empty.h>
 #include <nav_msgs/Path.h>
+#include <geometry_msgs/PointStamped.h>
 
 class PlanExecutor
 {
@@ -15,17 +16,17 @@ public:
   PlanExecutor(ros::NodeHandle nh, ros::NodeHandle nh_private) : action_client_("move_base", true)
 
   {
-    ROS_INFO_STREAM("Waiting for move_base action server to start.");
-    action_client_.waitForServer();
-    ROS_INFO_STREAM("move_base action server started.");
     map_sub_ = nh.subscribe("map_topic", 10, &PlanExecutor::mapCallback, this);
     start_sub_ = nh.subscribe("start_topic", 10, &PlanExecutor::startPoseCallback, this);
     goal_sub_ = nh.subscribe("goal_topic", 10, &PlanExecutor::goalPoseCallback, this);
+    clicked_point_sub_ = nh.subscribe("clicked_point", 10, &PlanExecutor::clickedPointCallback, this);
     path_pub_ = nh.advertise<nav_msgs::Path>("path", 10);
     timer_ = nh.createTimer(ros::Duration(0.1), &PlanExecutor::publishPlan, this);
 
     plan_service_ = nh.advertiseService("generate_plan", &PlanExecutor::planServiceCallback, this);
     execute_service_ = nh.advertiseService("execute_plan", &PlanExecutor::executeServiceCallback, this);
+    get_start_service_ = nh.advertiseService("get_start_pose", &PlanExecutor::getStartServiceCallback, this);
+    get_goal_service_ = nh.advertiseService("get_goal_pose", &PlanExecutor::getGoalServiceCallback, this);
 
     start_.pose.position.x = 0;
     start_.pose.position.y = 1;
@@ -33,7 +34,19 @@ public:
     goal_.pose.position.x = 1;
     goal_.pose.position.y = 0;
 
+    ROS_INFO_STREAM("Global Planner: Waiting for move_base action server to start.");
+    action_server_initialized_ = action_client_.waitForServer(ros::Duration(5.0));
+    if (action_server_initialized_)
+    {
+      ROS_INFO_STREAM("Global Planner: move_base action server started.");
+    }
+    else
+    {
+      ROS_WARN_STREAM("Global Planner: Waiting for action server timed out. Local Planning will be disabled.");
+    }
+
     plan_made_ = false;
+    received_map_ = false;
   }
 
   bool planServiceCallback(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
@@ -46,20 +59,53 @@ public:
     }
     else
     {
+      ROS_ERROR_STREAM("Global Planner: The map has not been received yet. Cannot generate plan.");
       return false;
     }
   }
 
   bool executeServiceCallback(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
   {
-    if (plan_made_)
+    if (plan_made_ && action_server_initialized_)
     {
       executePlan();
       return true;
     }
     else
     {
+      ROS_ERROR_STREAM("Global Planner: The plan has not been made yet, or the action server has not been initialized. "
+                       "Cannot execute plan");
       return false;
+    }
+  }
+
+  bool getStartServiceCallback(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
+  {
+    mode_ = MODE::GETSTARTPOSE;
+    ROS_INFO("Global Planner: Waiting for Start Pose");
+    return true;
+  }
+
+  bool getGoalServiceCallback(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
+  {
+    mode_ = MODE::GETGOALPOSE;
+    ROS_INFO("Global Planner: Waiting for Goal Pose");
+    return true;
+  }
+
+  void clickedPointCallback(const geometry_msgs::PointStamped& point)
+  {
+    if (mode_ == MODE::GETGOALPOSE)
+    {
+      goal_.pose.position = point.point;
+      mode_ = MODE::IDLE;
+      ROS_INFO("Global Planner: Received Goal pose");
+    }
+    else if (mode_ == MODE::GETSTARTPOSE)
+    {
+      start_.pose.position = point.point;
+      mode_ = MODE::IDLE;
+      ROS_INFO("Global Planner: Received Start pose");
     }
   }
 
@@ -104,7 +150,7 @@ public:
       move_base_msgs::MoveBaseGoal goal;
       goal.target_pose = pose;
       goal.target_pose.header.frame_id = "map";
-      
+
       ROS_INFO_STREAM("Sending new  ");
       ROS_INFO_STREAM(goal.target_pose);
 
@@ -117,7 +163,6 @@ public:
   void publishPlan(const ros::TimerEvent& event)
   {
     nav_msgs::Path path;
-    ROS_ERROR("PUBLISHING PATH");
     static long int seq = 0;
     for (int i = 0; i < plan_.size(); i++)
     {
@@ -133,6 +178,13 @@ public:
   }
 
 private:
+  enum class MODE
+  {
+    GETSTARTPOSE,
+    GETGOALPOSE,
+    IDLE
+  };
+
   actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> action_client_;
   grid_map::GridMap map_;
   mbf_rrts_core::RRTSPlanner planner_;
@@ -142,14 +194,19 @@ private:
   ros::Subscriber start_sub_;
   ros::Subscriber goal_sub_;
   ros::Subscriber map_sub_;
+  ros::Subscriber clicked_point_sub_;
   ros::ServiceServer plan_service_;
   ros::ServiceServer execute_service_;
+  ros::ServiceServer get_goal_service_;
+  ros::ServiceServer get_start_service_;
   ros::Publisher path_pub_;
 
   std::vector<geometry_msgs::PoseStamped> plan_;
   bool received_map_;
   bool plan_made_;
+  bool action_server_initialized_;
   ros::Timer timer_;
+  MODE mode_;
 };
 
 int main(int argc, char** argv)
